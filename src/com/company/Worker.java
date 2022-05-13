418ea612862c9b75b37d11023e9eaa58a2234983
Vasilisa Nichipor <vsilfox@gmail.com>
2022-05-13 19:23:46 +0300

    First commit

---
+++ src/com/company/Worker.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 32) {
+                errorCallback.onError("Task " + (i + 1) + " was finished with error");
+                continue;
+            }
+            callback.onDone("Task " + (i + 1) + " is done");
+        }
+    }
+}
